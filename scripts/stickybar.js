fb3f69131d0da2d8bf9b6efd4171d87a590f78a7
simenO <42930807+simenO@users.noreply.github.com>
2018-11-07 13:58:39 +0100

    Add stickybar.js

---
+++ scripts/stickybar.js
@@ -0,0 +1,20 @@
+
+var navbar = document.getElementById("nav");
+//lagrer hvor langt fra toppen av browservinduet navbar er
+var sticky = navbar.offsetTop;
+
+function scroll() {
+  //hvis man har scrollet lenger enn navbar er fra toppen
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky") //legger til class="sticky" i html
+    //.sticky er allerede lagt til i CSS filen.
+  }
+  else {
+    navbar.classList.remove("sticky") //ellers fjerner
+  }
+}
+
+//funksjonen kjører når man scroller
+window.onscroll = function() {
+  scroll()
+};

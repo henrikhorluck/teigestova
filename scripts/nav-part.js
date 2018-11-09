a81e5906c8b6d5b8144a5c297308943b85d50cdc
Henrik Hørlück Berg <henrik@horluck.no>
2018-11-09 16:29:46 +0100

    Change to how we want it to be

--- scripts/nav-part.js
+++ scripts/nav-part.js
@@ -9,7 +9,7 @@ navigation_bar.innerHTML = `<div id="navbar">
     <ul id="dropdown">
         <li><a href="teigestova.html">Hjem</a></li>
         <li><a href="about.html">Om oss</a></li>
-        <li id="hunder"><a href="ourDogs.html">Våre hunder<i class="fas fa-angle-down"></i></a>
+        <li id="hunder"><a id="ourDogs_link" href="#">Våre hunder<i class="fas fa-angle-down"></i></a>
               <ul id="under_hunder">
                     <li><a href="ourDogsParent.html?goldie">Goldie</a></li>
                     <li><a href="ourDogsParent.html?ariana">Ariana</a></li>
@@ -25,20 +25,46 @@ navigation_bar.innerHTML = `<div id="navbar">
 var dropdown = document.getElementById('dropdown')
 // TODO: ha landing page for våre hunder
 /* Åpner navbaren når den er liten */
-document.getElementById('navbar').addEventListener('click', function() {
+
+function open_dropdown() {
   if (dropdown.style.display == "block") {
     dropdown.style.display = "none"
   } else {
     dropdown.style.display = "block"
   }
-});
+};
 
 hunder = document.getElementById('hunder')
 under_hunder = document.getElementById('under_hunder')
+ourDogs_link = document.getElementById('ourDogs_link')
 
 /* Sjekker når nettleservinduet blir resiza, og endrer display-modusen til
 * dropdownen, slik at den vises riktig
 */
+function ourDogs_badlink(){
+  ourDogs_link.addEventListener('click', function(){
+    ourDogs_link.setAttribute('href', "ourDogs.html")
+  })
+  return false
+}
+
+
+function update_navbar_click(){ //LEGG TIL OPTIONAL VALUE, SLIK AT DEN IKKE HENTER INN VERDI TO GANGER
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  if (w > 760){
+    document.getElementById('navbar').removeEventListener('click', open_dropdown)
+    ourDogs_link.removeEventListener('click', ourDogs_badlink)
+    ourDogs_link.setAttribute('href', 'ourDogs.html')
+  } else if (w <= 760){
+    document.getElementById('navbar').addEventListener('click', open_dropdown)
+    ourDogs_link.addEventListener('click', ourDogs_badlink)
+    if (dropdown.style.display === 'none'){
+      ourDogs_link.setAttribute('href', "#")
+    }
+  }
+}
+update_navbar_click()
+
 window.onresize = function (){
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   if (w > 760){
@@ -46,4 +72,13 @@ window.onresize = function (){
   } else if (w <= 760){
     dropdown.style.display = "none"
   }
+  update_navbar_click()
 }
+
+hunder.addEventListener('click', function (){	
+  if (under_hunder.style.display != 'block') {	
+    under_hunder.style.display = 'block';	
+  } else {	
+    under_hunder.style.display = "none";	
+  }	
+})

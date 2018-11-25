fb2582abc637dc44a063f3569b2a16a4c56c5309
Henrik Hørlück Berg <henrik@horluck.no>
2018-11-25 01:39:16 +0100

    Move ourDogsParent to ourDogs, Make everything Ariana

--- scripts/nav-part.js
+++ scripts/nav-part.js
@@ -10,9 +10,9 @@ navigation_bar.innerHTML = `<div id="navbar">
         <li><a href="about.html">Om oss</a></li>
         <li id="hunder"><a id="ourDogs_link" href="#">Våre hunder ▾</a>
               <ul>
-                    <li class="under_hunder"><a href="ourDogsParent.html?goldie">Goldie</a></li>
-                    <li class="under_hunder"><a href="ourDogsParent.html?ariana">Ariana</a></li>
-                    <li class="under_hunder"><a href="ourDogsParent.html?whitney">Whitney</a></li>
+                    <li class="under_hunder"><a href="ourDogs.html?goldie">Goldie</a></li>
+                    <li class="under_hunder"><a href="ourDogs.html?arianna">Ariana</a></li>
+                    <li class="under_hunder"><a href="ourDogs.html?whitney">Whitney</a></li>
                     <li class="under_hunder"><a href="ourDogsPuppies.html">Valper</a></li>
                     <li class="under_hunder"><a href="ourDogsActivities.html">Aktiviteter</a></li>
               </ul>
